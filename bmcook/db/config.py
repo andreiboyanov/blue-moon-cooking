989bf6bf9e7ad276f2b3718d4c36f25938d0acf9
Andrei Boyanov <andrei.boyanov@gmail.com>
2023-06-06 13:02:03 +0200

    Adapt the env variables

--- bmcook/db/config.py
+++ bmcook/db/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-db_name = environ.get("PG_DB_NAME", "moon")
-db_user = environ.get("PG_DB_USER", "moon")
-db_password = environ.get("PG_DB_PASS", "moon")
-db_host = environ.get("PG_DB_HOST", "localhost")
-db_port = environ.get("PG_DB_PORT", 5432)
+db_name = environ.get("PG_DB", "moon")
+db_user = environ.get("PG_USER", "moon")
+db_password = environ.get("PG_PASS", "moon")
+db_host = environ.get("PG_HOST", "localhost")
+db_port = environ.get("PG_PORT", 5432)
